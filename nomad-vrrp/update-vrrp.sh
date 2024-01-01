7762c04c04a9bff67d7a17e42ed81ea75e363bc9
Mark Tannert <mark@tannert.net>
2024-01-01 16:08:16 -0500

    updates

--- nomad-vrrp/update-vrrp.sh
+++ nomad-vrrp/update-vrrp.sh
@@ -4,15 +4,22 @@ INTERFACE="enp0s3"
 
 IP_ADDRESS=$(/usr/sbin/ifconfig ${INTERFACE} | /usr/bin/grep 'inet' | /usr/bin/grep 'netmask' | /usr/bin/grep 'broadcast' | /usr/bin/cut -d' ' -f10)
 
-# Clear all NAT rules
-iptables -t nat -F
+# Update iptables
+if [ -d /mnt/vagabond/services/nomad-vrrp/iptables ]; then
+  for n in $(iptables --line-number -t nat -nL | grep DNAT | grep vrrp | cut -d ' ' -f1); do
+    iptables -t nat -D PREROUTING $n
+  done
+  for n in $(iptables --line-number -t nat -nL | grep SNAT | grep vrrp | cut -d ' ' -f1); do
+    iptables -t nat -D POSTROUTING $n
+  done
 
-# Unbound
-VRRP_ADDRESS=10.0.3.201
-VRRP_PORT=53
-/usr/sbin/iptables -t nat -A PREROUTING -d ${VRRP_ADDRESS}/32 -p udp -m udp --dport ${VRRP_PORT} -j DNAT --to-destination ${IP_ADDRESS}:
-/usr/sbin/iptables -t nat -A POSTROUTING -d ${IP_ADDRESS}/32 -p udp -m udp --dport ${VRRP_PORT} -j SNAT --to-source ${VRRP_ADDRESS}
+  for filename in /mnt/vagabond/services/nomad-vrrp/iptables/*.fwd; do
+    source $filename
+  done
+fi
 
-# Sync Changes
-rsync --recursive --delete /mnt/vagabond/services/nomad-vrrp/keepalived/* /etc/keepalived/
-systemctl restart keepalived.service
+# Update keepalived
+if [ -d /mnt/vagabond/services/nomad-vrrp/keepalived/ ]; then
+  rsync --recursive --delete /mnt/vagabond/services/nomad-vrrp/keepalived/* /etc/keepalived/
+  systemctl restart keepalived.service
+fi
