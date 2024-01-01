7dc56b239960788f6b7ef2752f1f93c6a1b12201
Mark Tannert <mark@tannert.net>
2024-01-01 16:43:07 -0500

    update

--- nomad-vrrp/nomad-vrrp.sh
+++ nomad-vrrp/nomad-vrrp.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-INTERFACE="enp0s3"
+while [ ! -f /mnt/vagabond/services/nomad-vrrp/update-vrrp.sh ]; do
+  sleep 5
+done
+/mnt/vagabond/services/nomad-vrrp/update-vrrp.sh
+echo "Ran Update at startup"
 
+
+INTERFACE="enp0s3"
 HOSTNAME=$(hostname)
 IP_ADDRESS=$(/usr/sbin/ifconfig ${INTERFACE} | /usr/bin/grep 'inet' | /usr/bin/grep 'netmask' | /usr/bin/grep 'broadcast' | /usr/bin/cut -d' ' -f10)
 SALT="tnbctvavpeowpenvrxatdnjvotamqbkq"
 HASH=$(echo "${HOSTNAME},${IP_ADDRESS},${SALT}" | sha256sum | cut -f1 -d' '
+
 
 while :
 do
