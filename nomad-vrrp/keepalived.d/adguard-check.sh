1ee97d5d6e3325e5d5eae1c8d644051c25bd96c1
Digital Dann <86263298+digital-dann@users.noreply.github.com>
2024-01-10 11:28:26 -0500

    Update and rename unbound.sh to adguard-check.sh

--- nomad-vrrp/keepalived.d/adguard-check.sh
+++ nomad-vrrp/keepalived.d/adguard-check.sh
@@ -3,7 +3,7 @@
 INTERFACE="eno1"
 
 IP_ADDRESS=$(ifconfig $INTERFACE | grep 'inet' | grep 'netmask' | grep 'broadcast' | cut -d' ' -f10)
-RESULT=$(dig -t TXT health.check @${IP_ADDRESS} | grep abcdefghijklmnopqrstuvwxyz | wc -l)
+RESULT=$(dig health.check @${IP_ADDRESS} | grep 169.254.254.254 | wc -l)
 if [[ "$RESULT" == "1" ]]; then
   exit 0
 fi
