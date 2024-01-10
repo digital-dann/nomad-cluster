563cd4eb1e436843cd317d8dca9b460c321a40a7
Digital Dann <86263298+digital-dann@users.noreply.github.com>
2024-01-10 11:27:28 -0500

    Update update-vrrp.sh

--- nomad-vrrp/update-vrrp.sh
+++ nomad-vrrp/update-vrrp.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-INTERFACE="eno1"
-IP_ADDRESS=$(/usr/sbin/ifconfig ${INTERFACE} | /usr/bin/grep 'inet' | /usr/bin/grep 'netmask' | /usr/bin/grep 'broadcast' | /usr/bin/cut -d' ' -f10)
-
-# Update iptables
-if [ -d /mnt/vagabond/services/nomad-vrrp/iptables.d ]; then
-  for n in $(echo $(iptables --line-number -t nat -nL | grep DNAT | grep vrrp | cut -d ' ' -f1) | rev); do
-    iptables -t nat -D PREROUTING $n
-  done
-  for n in $(echo $(iptables --line-number -t nat -nL | grep SNAT | grep vrrp | cut -d ' ' -f1) | rev); do
-    iptables -t nat -D POSTROUTING $n
-  done
-
-  for filename in /mnt/vagabond/services/nomad-vrrp/iptables.d/*.fwd; do
-    source $filename
-  done
-fi
-
 # Update keepalived
 if [ -d /mnt/vagabond/services/nomad-vrrp/keepalived.d/ ]; then
   rsync --recursive --delete /mnt/vagabond/services/nomad-vrrp/keepalived.d/* /etc/keepalived/
