4b35112f7ae928f55dfb06a9a16d2347f98a34af
Digital Dann <86263298+digital-dann@users.noreply.github.com>
2024-01-10 11:24:48 -0500

    Update deploy.sh

--- nomad-vrrp/deploy.sh
+++ nomad-vrrp/deploy.sh
@@ -12,7 +12,7 @@ if [ ! -d ./nodes.d ]; then
   mkdir ./nodes.d
 fi
 
-for n in arrNodes; do
+for n in ${arrNodes[@]}; do
   HASH=$(echo "${n},${SALT}" | sha256sum | cut -f1 -d' ')
   touch ./nodes.d/${HASH}
 done
