16820ed9ef0936a32ba5c3a674ec2812950c3d5f
Digital Dann <86263298+digital-dann@users.noreply.github.com>
2024-01-17 15:00:39 -0500

    Update adguard-check.sh

--- nomad-vrrp/keepalived.d/adguard-check.sh
+++ nomad-vrrp/keepalived.d/adguard-check.sh
@@ -3,7 +3,7 @@
 INTERFACE="eno1"
 
 IP_ADDRESS=$(ifconfig $INTERFACE | grep 'inet' | grep 'netmask' | grep 'broadcast' | cut -d' ' -f10)
-RESULT=$(dig adguard.service.consul @169.254.254.254 -p 8600 | "^adguard.service.consul." | cut -d$'\t' -f5)
+RESULT=$(dig adguard.service.consul @169.254.254.254 -p 8600 | grep "^adguard.service.consul." | cut -d$'\t' -f5)
 if [[ "$RESULT" == "$IP_ADDRESS" ]]; then
   exit 0
 fi
